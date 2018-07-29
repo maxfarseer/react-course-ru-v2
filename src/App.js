c0006e1aa3bab6efb7b84425c3860b04582b4bc3
Max P <maxfarseer@gmail.com>
2018-07-29 11:05:36 +0300

    add (App.js) async call for json data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import { Add } from './components/Add'
 import { News } from './components/News'
-import newsData from './data/newsData'
+// удален импорт newsData
 import './App.css'
 
 class App extends React.Component {
   state = {
-    news: newsData,
+    news: null,
+    isLoading: false,
+  }
+  componentDidMount() {
+    this.setState({ isLoading: true })
+    fetch('http://localhost:3000/data/newsData.json')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setTimeout(() => {
+          this.setState({ isLoading: false, news: data })
+        }, 3000)
+      })
   }
   handleAddNews = data => {
     const nextNews = [data, ...this.state.news]
     this.setState({ news: nextNews })
   }
   render() {
+    const { news, isLoading } = this.state
+
     return (
       <React.Fragment>
         <Add onAddNews={this.handleAddNews} />
         <h3>Новости</h3>
-        <News data={this.state.news} />
+        {isLoading && <p>Загружаю...</p>}
+        {Array.isArray(news) && <News data={news} />}
       </React.Fragment>
     )
   }
